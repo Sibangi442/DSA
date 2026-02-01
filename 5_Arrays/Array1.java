08653ab232a056485e5fc86f29a32605341b2200
Sibangi442 <sibangipadhi93@gmail.com>
2026-02-02 05:03:35 +0530

    create an array

---
+++ 5_Arrays/Array1.java
@@ -0,0 +1,27 @@
+public class Array1 {
+    public static void main(String args[]){
+        int marks[]= new int[50];
+
+        // int numbers[]={1,2,3};
+
+        // int marks[]=new int[100];
+        // Scanner sc = new Scanner(System.in);
+        // marks[0]=sc.nextInt();
+        // marks[1]=sc.nextInt();
+        // marks[2]=sc.nextInt();
+        // System.out.println("phy "+marks[0]);
+        // System.out.println("chem "+marks[1]);
+        // System.out.println("math "+marks[2]);
+
+        // // marks[2]=100;
+        // // marks[2]=marks[2]+1;
+        // // System.out.println("Math " +marks[2]);
+
+        // int percentage = (marks[0]+marks[1]+marks[2] )/3;
+        // System.out.println("percentage = "+percentage +"%");
+
+        //to find the length of array
+        System.out.println("Length of array "+marks.length);
+
+    }
+}
