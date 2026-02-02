1054080cc4002ee14db2cbc63689bba95bf099a3
Sibangi442 <sibangipadhi93@gmail.com>
2026-02-02 05:53:49 +0530

    Linear Search

---
+++ 5_Arrays/Array3.java
@@ -0,0 +1,20 @@
+public class Array3 {
+    public static int linearsearch(int numbers[], int key){
+        for(int i = 0;i<numbers.length;i++){
+            if(numbers[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10,1,2,14};
+        int key = 19;
+         int index = linearsearch(numbers, key);
+        if(index ==-1){
+            System.out.println("Key not found");
+        } else{
+             System.out.println(index);
+        }
+    }
+}
