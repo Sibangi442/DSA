186eac028253cffcbc9ef6323523f65e4185e962
Sibangi442 <sibangipadhi93@gmail.com>
2026-02-03 05:10:46 +0530

    Smallest and largest value in an array

---
+++ 5_Arrays/Array4.java
@@ -0,0 +1,21 @@
+public class Array4 {
+    public static int getlargest(int numbers[]){
+        int largest = Integer.MIN_VALUE; //-infinity
+        int smallest = Integer.MAX_VALUE;
+
+        for(int i = 0;i<numbers.length;i++){
+            if(largest<numbers[i]){
+                largest = numbers[i];
+            }
+            if(smallest>numbers[i]){
+                smallest = numbers[i];
+            }
+        }
+        System.out.println("The smallest value is "+smallest);
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,2,6,3,5};
+        System.out.println("The largets value is "+getlargest(numbers));
+    }
+}
