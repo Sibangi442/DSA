68bc4baf177b64c32d9c83cbf30d95289e8a6daa
Sibangi442 <sibangipadhi93@gmail.com>
2026-02-06 23:16:43 +0530

    Array practice

---
+++ 5_Arrays/Array_Practice.java
@@ -0,0 +1,76 @@
+import java.util.*;
+public class Array_Practice {
+
+    //Linear search
+    public static int linear(int arr[],int key){
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Largest Value
+    public static int largest(int arr[]){
+        // int largest = arr.MIN_VALUE;
+        int largest = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+        return largest;
+    }
+
+    //SMALLEST VALUE
+    public static int smallest(int arr[]){
+        int small = Integer.MAX_VALUE;
+        for(int i =0;i<arr.length;i++){
+             if(small>arr[i]){
+            small = arr[i];
+        }
+        }
+        return small;
+    }
+
+    //BINARY SEARCH
+    public static int Binary(int arr[], int key){
+        int start = 0;
+        int end = arr.length- 1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]>key){
+                end = mid -1;
+            } else{
+                start = mid
+            }
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+    //     // Operations in an array -Create an Array
+
+    //     // int arr[]= {1,2,3,4,5,6};
+
+    //     int arr[]= new int[10];
+    //     for(int i = 0;i<arr.length;i++){
+    //         // System.out.println(arr[i]);
+    //         arr[i]= sc.nextInt();
+    //     }
+    //     System.out.println("The array is ");
+    //     for(int i = 0;i<arr.length;i++){
+    //         System.out.print(arr[i]);
+        
+    // }
+
+     int arr[]=new int[5];
+     for(int i = 0;i<arr.length;i++){
+        arr[i]= sc.nextInt();
+     }
+     int key = 6;
+    //  System.out.println("The key is at index "+linear(arr,key));
+      System.out.println("The largest value is "+ largest(arr));
+       System.out.println("The smalllest value is "+smallest(arr));
+}
+}
