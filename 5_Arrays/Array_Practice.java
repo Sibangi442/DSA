bfd5b4774977b19e9bfe2903aa0e3dd70acc3fd5
Sibangi442 <sibangipadhi93@gmail.com>
2026-02-07 22:50:11 +0530

    added the First Occurence for binary search

--- 5_Arrays/Array_Practice.java
+++ 5_Arrays/Array_Practice.java
@@ -40,13 +40,38 @@ public class Array_Practice {
         int end = arr.length- 1;
         while(start<=end){
             int mid = (start+end)/2;
+            if(arr[mid]==key){
+                return mid;
+            }
             if(arr[mid]>key){
                 end = mid -1;
             } else{
-                start = mid
+                start = mid+1;
             }
         }
+        return -1;
     }
+
+    //First Occurence of the Binary Search 
+    public static int firstOccurrence(int arr[], int key){
+        int start = 0, end = arr.length-1;
+        int ans = -1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==key){
+                ans = mid;
+                end = mid-1;
+            }
+             else if(arr[mid]> key){
+                end = mid-1;
+            } else{
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
     //     // Operations in an array -Create an Array
@@ -68,9 +93,11 @@ public class Array_Practice {
      for(int i = 0;i<arr.length;i++){
         arr[i]= sc.nextInt();
      }
-     int key = 6;
+     int key = 4;
     //  System.out.println("The key is at index "+linear(arr,key));
-      System.out.println("The largest value is "+ largest(arr));
-       System.out.println("The smalllest value is "+smallest(arr));
+    //   System.out.println("The largest value is "+ largest(arr));
+    //    System.out.println("The smalllest value is "+smallest(arr));
+    // System.out.println("The value is at index "+Binary(arr, key));
+    System.out.println("The value is at index "+firstOccurrence(arr, key));
 }
 }
