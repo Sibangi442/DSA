b07822c379211317e9f53d80656178157e8eeb51
Sibangi442 <sibangipadhi93@gmail.com>
2026-05-05 12:32:24 +0530

    .

--- 9_Strings/String9.java
+++ 9_Strings/String9.java
@@ -3,12 +3,12 @@ public class String9 {
         String newstr ="";
         for(int i= 0;i<str.length();i++){
             Integer count = 1;
-            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){ //it will work till and unless string is less than length and current value and nest value are equal
                 count++;
                 i++;
             }
-            newstr+= str.charAt(i);
-            if(count>1){
+            newstr+= str.charAt(i); //the string value gets added to the newstring 
+            if(count>1){ //when count value is greater than 1 then only count gets added to the newstring   
                 newstr +=count.toString(); //add the count to the string like = a3
             }
         }
