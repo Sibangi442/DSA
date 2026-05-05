86c16e731b52c545ac342622a99832c1af9abee5
Sibangi442 <sibangipadhi93@gmail.com>
2026-05-05 12:20:55 +0530

    String Compression

---
+++ 9_Strings/String9.java
@@ -0,0 +1,21 @@
+public class String9 {
+    public static String compress(String str){
+        String newstr ="";
+        for(int i= 0;i<str.length();i++){
+            Integer count = 1;
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            newstr+= str.charAt(i);
+            if(count>1){
+                newstr +=count.toString(); //add the count to the string like = a3
+            }
+        }
+        return newstr;
+    }
+    public static void main(String args[]){
+        String str = "aaabbcccdd";
+        System.out.println(compress(str));
+    }
+}
