4a4bf46a8e982b1b90be17060e265a44c7b30c34
Sibangi442 <sibangipadhi93@gmail.com>
2026-05-05 18:31:59 +0530

    using normal as well as String Builder

--- 9_Strings/String9.java
+++ 9_Strings/String9.java
@@ -1,18 +1,37 @@
 public class String9 {
+    // public static String compress(String str){
+    //     String newstr ="";
+    //     for(int i= 0;i<str.length();i++){
+    //         Integer count = 1;
+    //         while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){                       //it will work till and unless string is less than length and current value and nest value are equal
+    //             count++;
+    //             i++;
+    //         }
+    //         newstr+= str.charAt(i);                   //the string value gets added to the newstring 
+    //         if(count>1){                              //when count value is greater than 1 then only count gets added to the newstring   
+    //             newstr +=count.toString();             //add the count to the string like = a3
+    //         }
+    //     }
+    //     return newstr;
+    // }
+
+
+
+    // USING STRINGBUILDER 👇
     public static String compress(String str){
-        String newstr ="";
-        for(int i= 0;i<str.length();i++){
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0;i<str.length()-1;i++){
             Integer count = 1;
-            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){ //it will work till and unless string is less than length and current value and nest value are equal
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
                 count++;
                 i++;
             }
-            newstr+= str.charAt(i); //the string value gets added to the newstring 
-            if(count>1){ //when count value is greater than 1 then only count gets added to the newstring   
-                newstr +=count.toString(); //add the count to the string like = a3
+            sb.append(str.charAt(i));
+            if(count>1){
+                sb.append(count.toString());
             }
         }
-        return newstr;
+        return sb.toString();
     }
     public static void main(String args[]){
         String str = "aaabbcccdd";
