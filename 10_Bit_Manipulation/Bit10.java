17734f06350865246ab461c3978cb1b9e399ab10
Sibangi442 <sibangipadhi93@gmail.com>
2026-05-07 19:50:31 +0530

    count set bits

---
+++ 10_Bit_Manipulation/Bit10.java
@@ -0,0 +1,15 @@
+public class Bit10 {
+    public static int countsetbits(int n){
+        int count = 0;
+        while(n>0){
+           if((n&1)!=0){ //check our least significant digit
+            count++;
+           }
+           n =n>>1; 
+        }
+        return count;
+    }
+    public static void main(String args[]){
+        System.out.println(countsetbits(10));
+    }
+}
