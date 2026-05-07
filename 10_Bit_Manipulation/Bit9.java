83de46831f4ba3c851e135c5df409f3753829685
Sibangi442 <sibangipadhi93@gmail.com>
2026-05-07 18:25:54 +0530

    power of 2 or not

---
+++ 10_Bit_Manipulation/Bit9.java
@@ -0,0 +1,8 @@
+public class Bit9 {
+    public static boolean ispowerof2(int n){
+        return (n & (n-1))==0;
+    }
+    public static void main(String args[]){
+        System.out.println(ispowerof2(15));
+    }
+}
