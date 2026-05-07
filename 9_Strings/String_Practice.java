6070192a6a30d4b52e1fb780db0ee1622ce84558
Sibangi442 <sibangipadhi93@gmail.com>
2026-05-07 12:09:25 +0530

    practice

--- 9_Strings/String_Practice.java
+++ 9_Strings/String_Practice.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class String_Practice {
     public static void printLetters(String str){
         for(int i = 0;i<str.length();i++){
-            System.out.print(str.charAt(i)+" ");
+            System.out.print(str.charAt(i)+"  ");
         }
         System.out.println();
     }
