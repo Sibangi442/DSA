f82d72ae57ae6ad80f933235c8bde10c3dfa6d83
Sibangi442 <sibangipadhi93@gmail.com>
2026-05-07 16:05:36 +0530

    clear bits in range

---
+++ 10_Bit_Manipulation/Bit8.java
@@ -0,0 +1,11 @@
+public class Bit8 {
+    public static int clearBitsInRange(int n, int i, int j){
+        int a = (~0)<<(j+1);
+        int b = (1<<i)-1;
+        int bitmask = a|b;
+        return n & bitmask;
+    }
+    public static void main(String args[]){
+        System.out.println(clearBitsInRange(10,1,2));
+    }
+}
