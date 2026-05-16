650de70989d141236228e2db38791d5798fe02db
Sibangi442 <sibangipadhi93@gmail.com>
2026-05-16 07:05:18 +0530

    Packages

---
+++ 11_OOPS/Oops14.java
@@ -0,0 +1,9 @@
+
+import java.util.*;  // utility package of java , where the scanner is also present in this package
+
+public class Oops14 {
+    public  static void main(String args[]){
+        Scanner sc= new Scanner(System.in);  // here scanner is a class whose object we have created that is , sc  with the help of new keyword , and the scanner() constructor is beibg called
+        int a = sc.nextInt();
+    }
+}
